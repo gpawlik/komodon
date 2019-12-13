94ffe27f4c6285279d52d7a480d64a9210daa935
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2019-12-13 18:17:41 +0000

    Add iata codes

--- src/screens/results/components/result-box/index.js
+++ src/screens/results/components/result-box/index.js
@@ -28,7 +28,9 @@ import {
 
 export const ResultBox = ({ price, routes = [], deepLink = '' }) => {
     const openLink = () => Linking.openURL(deepLink).catch(err => console.log('An error occurred', err));
-    const airlineList = routes.map(({ airlines = [] }) => airlines.map(iata => airlineCodes[iata]));
+    const airlineList = routes.map(({ airlines = [] }) =>
+        airlines.map(iata => (airlineCodes[iata] || iata).slice(0, 20)),
+    );
     const uniqueAirlines = R.compose(R.uniq, R.flatten)(airlineList);
 
     return (
