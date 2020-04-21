81fd3853b290e642b41074c133986e97f5f50b15
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2020-04-21 21:08:05 +0100

    Fix navigation on refresh

--- src/screens/search-date/index.tsx
+++ src/screens/search-date/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import * as R from 'ramda';
 import { connect } from 'react-redux';
-//import { useLocation } from 'react-router-dom';
 
 import {
     getDepartureDates,
